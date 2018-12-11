086067bd6171f93ce5fedf38b88283cd80236bf3
bryan-onwas <bryan.onwas@gmail.com>
2018-12-10 22:49:43 -0600

    Removed local storage script for now

--- scripts/main.js
+++ scripts/main.js
@@ -9,25 +9,24 @@ navBtn.addEventListener('click', navButton);
 function navButton() {
   navBtn.children[0].classList.toggle('is-active');
   sideBar.classList.toggle('sidebar__open');
-
 }
 
 // Function Calls
-document.body.style.backgroundColor = sessionStorage.getItem('bg');
-document.body.style.color = sessionStorage.getItem('cc');
-function darker() {
-  if (sessionStorage.getItem('bg') === 'rgb(255, 255, 255)') {
-    sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
-    sessionStorage.setItem('cc', '#777');
-  } else if (sessionStorage.getItem('bg') == null || undefined) {
-    sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
-    sessionStorage.setItem('cc', '#777');
-  } else if (sessionStorage.getItem('bg') === 'rgb(6, 23, 37)') {
-    sessionStorage.setItem('bg', 'rgb(255, 255, 255)');
-    sessionStorage.setItem('cc', '#333');
-  }
+// document.body.style.backgroundColor = sessionStorage.getItem('bg');
+// document.body.style.color = sessionStorage.getItem('cc');
+// function darker() {
+//   if (sessionStorage.getItem('bg') === 'rgb(255, 255, 255)') {
+//     sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
+//     sessionStorage.setItem('cc', '#777');
+//   } else if (sessionStorage.getItem('bg') == null || undefined) {
+//     sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
+//     sessionStorage.setItem('cc', '#777');
+//   } else if (sessionStorage.getItem('bg') === 'rgb(6, 23, 37)') {
+//     sessionStorage.setItem('bg', 'rgb(255, 255, 255)');
+//     sessionStorage.setItem('cc', '#333');
+//   }
 
-  document.body.style.backgroundColor = sessionStorage.getItem('bg');
-  document.body.style.color = sessionStorage.getItem('cc');
-}
-darker();
+//   document.body.style.backgroundColor = sessionStorage.getItem('bg');
+//   document.body.style.color = sessionStorage.getItem('cc');
+// }
+// darker();
