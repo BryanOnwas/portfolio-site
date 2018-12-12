7caeeb96ba96ebc58a2928a88e7458dcef6aed01
bryan-onwas <bryan.onwas@gmail.com>
2018-12-12 11:29:20 -0600

    Morning update to website

--- scripts/main.js
+++ scripts/main.js
@@ -11,22 +11,4 @@ function navButton() {
   sideBar.classList.toggle('sidebar__open');
 }
 
-// Function Calls
-// document.body.style.backgroundColor = sessionStorage.getItem('bg');
-// document.body.style.color = sessionStorage.getItem('cc');
-// function darker() {
-//   if (sessionStorage.getItem('bg') === 'rgb(255, 255, 255)') {
-//     sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
-//     sessionStorage.setItem('cc', '#777');
-//   } else if (sessionStorage.getItem('bg') == null || undefined) {
-//     sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
-//     sessionStorage.setItem('cc', '#777');
-//   } else if (sessionStorage.getItem('bg') === 'rgb(6, 23, 37)') {
-//     sessionStorage.setItem('bg', 'rgb(255, 255, 255)');
-//     sessionStorage.setItem('cc', '#333');
-//   }
-
-//   document.body.style.backgroundColor = sessionStorage.getItem('bg');
-//   document.body.style.color = sessionStorage.getItem('cc');
-// }
-// darker();
+// Function Calls (if any)
