abd18f02094a1322df3f85df7161b831ac58d7cf
bryan-onwas <bryan.onwas@gmail.com>
2018-12-17 02:52:33 -0600

    Fixed design/event handling issues on IE and improved website

--- js/main.js
+++ js/main.js
@@ -11,7 +11,11 @@ var body = document.querySelector('body');
 var lists = document.querySelectorAll('.sidebar--menu li');
 var moblie = window.matchMedia('(max-width: 768px)');
 
-var scroll = new SmoothScroll('a[href*="#"]');
+// Init functionality from dependencies
+var scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 500
+});
+
 new WOW().init();
 
 // Event Handlers
@@ -57,21 +61,27 @@ function backButton() {
   }
 }
 
-// Change which Y-position the top button is visible/invisible
+// Check where (y-position) the top button should be visible/invisible
 function topButton() {
+  // Check where (y-position) the top button should be visible
   if (window.pageYOffset >= 150) {
     topBtn.classList.add('btn--top__active');
-  } else if (window.pageYOffset < 100) {
+  }
+
+  // Otherwise, check which Y-position the top button should be invisible
+  else if (window.pageYOffset < 100) {
     topBtn.classList.remove('btn--top__active');
   }
 }
 
-// Change header opacity/color when scrolled away from top
-// Revert header opacity/color when scrolled back to top
 function navTop() {
+  // Change header opacity/color when scrolled away from top
   if (window.pageYOffset > 0) {
     navBar.classList.add('active');
-  } else if (window.pageYOffset === 0) {
+  }
+
+  // Otherwise, revert header opacity/color when scrolled back to top
+  else if (window.pageYOffset === 0) {
     navBar.classList.remove('active');
   }
 }
@@ -84,12 +94,13 @@ function outsideClick() {
 // Listen for manual window resizing by user
 function Resize() {
   // Check if window is resized to moblie while the sidebar is open
-  // If true, remove the scroll bar
+  // Remove the scroll bar if that's the case
   if (document.body.clientWidth <= '768' && sideBar.className === 'open') {
     body.classList.add('active');
   }
 
-  // Otherwise, add the scroll bar back
+  // Check if window is resized to tablet/desktop while the sidebar is open
+  // Add the scroll bar back if that's the case
   else {
     body.classList.remove('active');
   }
